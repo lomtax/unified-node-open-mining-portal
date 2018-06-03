3642223c961779bb683cbaba86e28daac387077a
root <root@localhost.localdomain>
2018-06-03 18:27:04 +0000

    newadress update

--- miners/digitalcoin/newPaymentAddress.sh
+++ miners/digitalcoin/newPaymentAddress.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 pattern='"address": '
-newAddress=$(digitalcoin-cli getaccountaddress 0)
+scryptaddress=$(digitalcoin-cli -datadir=scrypt getaccountaddress 0)
+sha256address=$(digitalcoin-cli -datadir=sha256 getaccountaddress 0)
+x11address=$(digitalcoin-cli -datadir=x11 getaccountaddress 0)
 quote='"'
 
-sudo sed -i "s/$pattern.*/$pattern$quote$newAddress$quote,/" ../../pool_configs/./digitalcoin_scrypt.json
-sudo sed -i "s/$pattern.*/$pattern$quote$newAddress$quote,/" ../../pool_configs/./digitalcoin_sha256.json
-sudo sed -i "s/$pattern.*/$pattern$quote$newAddress$quote,/" ../../pool_configs/./digitalcoin_x11.json
+sudo sed -i "s/$pattern.*/$pattern$quote$scryptaddress$quote,/" ../../pool_configs/./digitalcoin_scrypt.json
+sudo sed -i "s/$pattern.*/$pattern$quote$sha256address$quote,/" ../../pool_configs/./digitalcoin_sha256.json
+sudo sed -i "s/$pattern.*/$pattern$quote$x11address$quote,/" ../../pool_configs/./digitalcoin_x11.json
 
